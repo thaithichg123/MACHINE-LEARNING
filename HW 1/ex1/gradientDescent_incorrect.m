4b2721a81a6e7f7674003d665f6fb7af79c661fe
thaithichg123 <36321686+thaithichg123@users.noreply.github.com>
2021-02-07 00:43:31 -0600

    Add files via upload

---
+++ HW 1/ex1/gradientDescent_incorrect.m
@@ -0,0 +1,39 @@
+function [theta, J_history] = gradientDescent_incorrect(X, y, theta, alpha, num_iters)
+%GRADIENTDESCENT Performs gradient descent to learn theta
+%   theta = GRADIENTDESENT(X, y, theta, alpha, num_iters) updates theta by 
+%   taking num_iters gradient steps with learning rate alpha
+
+% Initialize some useful values
+m = length(y); % number of training examples
+J_history = zeros(num_iters, 1);
+
+for iter = 1:num_iters
+ H=X*theta;
+Total=H-y;
+theta(1)=theta(1)-((alpha/m)*(sum((Total.*X(:,1)))));
+ H=X*theta;
+Total=H-y;
+theta(2)=theta(2)-((alpha/m)*(sum((Total.*X(:,2)))));
+    % ====================== YOUR CODE HERE ======================
+    % Instructions: Perform a single gradient step on the parameter vector
+    %               theta. However you should update theta_0 fisrt, then
+    %               update theto_1.
+    %
+    % Hint: While debugging, it can be useful to print out the values
+    %       of the cost function (computeCost) and gradient here.
+    %
+
+
+    
+    
+    
+    
+    
+    % ============================================================
+
+    % Save the cost J in every iteration    
+    J_history(iter) = computeCost(X, y, theta);
+
+end
+
+end
